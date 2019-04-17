705ce1c7f6986e15a6b99957631c2fe06b4169ba
WangDaLei <wangcppclei@gmail.com>
2019-04-18 00:20:46 +0800

    add client

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 
 import json
 import time
+import random
 from socket import socket, AF_INET, SOCK_STREAM
 
 
@@ -38,6 +39,25 @@ class Client():
                 self.list_pai = sorted(list_recv)
                 break
 
+    def choose_from_pai(self, pai_list):
+        rm_list = []
+        for i in range(3):
+            len_pai = len(pai_list)
+            pos = random.randint(0, len_pai - 1)
+            rm_list.append(pai_list[pos])
+            pai_list.remove(pai_list[pos])
+        return rm_list
+
+    def make_policy_decision(self, policy_type, hand_pai):
+        if policy_type == 'exchange':
+            hand_pai = sorted(hand_pai.items(), key=lambda item: len(item[1]))
+            for key in hand_pai:
+                if len(hand_pai[key]) >= 3:
+                    three = self.choose_from_pai(hand_pai[key])
+                    return key, three
+        else:
+            pass
+
     def trible_charge(self, list_pai):
         tong_pai = []
         tiao_pai = []
@@ -51,6 +71,11 @@ class Client():
                 tiao_pai.append(num)
             else:
                 wan_pai.append(num)
+        hand_pai = {}
+        hand_pai['tong'] = tong_pai
+        hand_pai['tiao'] = tiao_pai
+        hand_pai['wan'] = wan_pai
+        key, three = self.make_policy_decision(list_pai, hand_pai)
 
     def decide(self):
         str_pai = ''
